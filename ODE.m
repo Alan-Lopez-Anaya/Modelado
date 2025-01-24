11f027c4e432eeadca51c3456ba3c2fc023c39b3
Alan-Lopez-Anaya <alan.lopez0379@alumnos.udg.mx>
2025-01-23 21:42:05 -0600

    ODE.m

--- ODE.m
+++ ODE.m
@@ -1,9 +1,16 @@
-[t,z]=ode45(@MRA,[0 100], [0, 0, deg2rad(1), 0]);
+[t,z]=ode45(@MRA,[0 10], [0, 0, deg2rad(1), 0]);
 
 figure(1)
 %subplot(2,1,1);
 plot(t,z(:,1));
 grid on 
 title("Posicion");
+xlabel("Tiempo");
+ylabel("Metros");
+
+subplot(2,1,2);
+plot(t,z(:,2));
+grid on 
+title("Velocidad");
 xlabel("Tiempo");
 ylabel("Metros");
